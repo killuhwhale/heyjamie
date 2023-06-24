dac2416dde1bc7da27271fbd4b22aa042170eed1
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-06-24 13:23:12 -0700

    host basePath

--- src/components/ActionCancelModal.tsx
+++ src/components/ActionCancelModal.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
-
 interface ActionCancelProps {
-    isOpen: boolean;
-    onAction(): void;
-    onClose(): void;
-    message: string;
-    note?: string;
+  isOpen: boolean;
+  onAction(): void;
+  onClose(): void;
+  message: string;
+  note?: string;
 }
-const ActionCancelModal: React.FC<ActionCancelProps> = ({ isOpen, onAction, onClose, message, note })  => {
-
-
-  console.log("isModalOpen", isOpen)
+const ActionCancelModal: React.FC<ActionCancelProps> = ({
+  isOpen,
+  onAction,
+  onClose,
+  message,
+  note,
+}) => {
   return (
     <>
       {isOpen && (
@@ -20,28 +22,31 @@ const ActionCancelModal: React.FC<ActionCancelProps> = ({ isOpen, onAction, onCl
             className="fixed inset-0 bg-gray-900 opacity-70"
             onClick={onClose}
           ></div>
-          <div className="bg-white p-4 rounded-md z-50 w-[600px] h-[350px] flex flex-col justify-between">
-            <h2 className="text-lg text-center justify-center  font-bold mb-2">Hey Jamie</h2>
-            <p className="p-4 text-lg text-center">{message}</p>
-            <p className="p-4 text-base text-center">{note}</p>
-            <div className="w-full flex justify-around">
-                <button
-                    onClick={onClose}
-                    className="w-1/3 bg-slate-600  hover:bg-slate-700 focus:bg-slate-700 active:bg-slate-800 text-white font-bold py-2 px-4 rounded">
+          <div className="z-50 flex h-[350px] w-[600px] flex-col justify-between rounded-md bg-white p-4">
+            <h2 className="mb-2 justify-center text-center  text-lg font-bold">
+              Hey Jamie
+            </h2>
+            <p className="p-4 text-center text-lg">{message}</p>
+            <p className="p-4 text-center text-base">{note}</p>
+            <div className="flex w-full justify-around">
+              <button
+                onClick={onClose}
+                className="w-1/3 rounded  bg-slate-600 px-4 py-2 font-bold text-white hover:bg-slate-700 focus:bg-slate-700 active:bg-slate-800"
+              >
                 Cancel
-                </button>
-                <button
-                    onClick={onAction}
-                    className="bg-blue-500 w-1/3  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              </button>
+              <button
+                onClick={onAction}
+                className="w-1/3 rounded  bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+              >
                 OK
-                </button>
-
+              </button>
             </div>
           </div>
         </div>
       )}
     </>
   );
-}
+};
 
 export default ActionCancelModal;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,29 @@ import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "../utils/api";
 import { useEffect, useRef, useState } from "react";
-import SpeechRecognition, { useSpeechRecognition, } from 'react-speech-recognition';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import { env } from "../env.mjs";
 import ActionCancelModal from "src/components/ActionCancelModal";
 
-
-
 class CancelablePromise<T> extends Promise<T> {
-  private cancelFn?: (() => void);
+  private cancelFn?: () => void;
 
-  constructor(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) {
+  constructor(
+    executor: (
+      resolve: (value?: T | PromiseLike<T>) => void,
+      reject: (reason?: any) => void
+    ) => void
+  ) {
     super((resolve, reject) => {
-      executor(
-        (value?: T | PromiseLike<T>) => {
-          if (this.cancelFn) {
-            reject('Promise canceled');
-          } else {
-            if(value) resolve(value);
-
-          }
-        },
-        reject
-      );
+      executor((value?: T | PromiseLike<T>) => {
+        if (this.cancelFn) {
+          reject("Promise canceled");
+        } else {
+          if (value) resolve(value);
+        }
+      }, reject);
     });
   }
 
@@ -35,7 +36,6 @@ class CancelablePromise<T> extends Promise<T> {
   }
 }
 
-console.log("ENV", process.env)
 interface QueryResultProps {
   question: string;
   voiceName: string;
@@ -45,23 +45,20 @@ interface QueryResultProps {
 
 /** To get better voice maybe setup another server for https://github.com/enhuiz/vall-e?ref=blog.paperspace.com */
 
-
 const QueryResult: React.FC<QueryResultProps> = (props) => {
   const getAnswer = api.example.openai.useMutation();
   const getSpeech = api.example.ttsGCP.useMutation();
-  const audioRef = useRef<HTMLAudioElement>()
-  const [playing, setPlaying] = useState(false)
-
-
-  useEffect(() => {
-    getAnswer.mutate({text: props.question })
-  }, [props.question])
-
+  const audioRef = useRef<HTMLAudioElement>();
+  const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
-    const answer = getAnswer.data?.answer || ""
-    if(!answer) return
-    getSpeech.mutate({text: answer, voiceName: props.voiceName})
+    getAnswer.mutate({ text: props.question });
+  }, [props.question]);
+
+  useEffect(() => {
+    const answer = getAnswer.data?.answer || "";
+    if (!answer) return;
+    getSpeech.mutate({ text: answer, voiceName: props.voiceName });
     // const sayMsg = async (msgToSpk: string) => {
     //   return new CancelablePromise((res, rej) => {
     //     console.log("useEffect QR: ", msgToSpk)
@@ -78,9 +75,6 @@ const QueryResult: React.FC<QueryResultProps> = (props) => {
     //     // msg.voice = window.speechSynthesis.getVoices()[9] || null // latin accent funny
     //     // msg.voice = window.speechSynthesis.getVoices()[10] || null // asian accent funny
     //     // msg.voice = window.speechSynthesis.getVoices()[11] || null // asian accent accurate
-
-
-
 
     //     if(!msgToSpk) return console.log("No msg to speak")
     //     msg.text = msgToSpk
@@ -118,11 +112,7 @@ const QueryResult: React.FC<QueryResultProps> = (props) => {
 
     //     }))
 
-
     //   }
-
-
-
 
     // return new CancelablePromise((res, rej) => {
     //   Promise.all(promises).then(() => {
@@ -146,15 +136,13 @@ const QueryResult: React.FC<QueryResultProps> = (props) => {
     // .catch(err => {
     //   console.log("Overall saymessages error: ", err)
     // })
-
-  }, [getAnswer.data?.answer])
-
+  }, [getAnswer.data?.answer]);
 
   useEffect(() => {
     // console.log("GCP Res ", getSpeech.data?.gcpRes)
-    if(!getSpeech.data?.gcpRes) return
-    const rawData = getSpeech.data?.gcpRes
-    if(!rawData) return
+    if (!getSpeech.data?.gcpRes) return;
+    const rawData = getSpeech.data?.gcpRes;
+    if (!rawData) return;
 
     const raw = window.atob(rawData);
     const rawLength = raw.length;
@@ -165,226 +153,259 @@ const QueryResult: React.FC<QueryResultProps> = (props) => {
     }
 
     const blob = new Blob([arr], {
-      type: 'audio/mp3'
+      type: "audio/mp3",
     });
     const blobUrl = URL.createObjectURL(blob);
     const audio = new Audio();
-    audioRef.current = audio
-    audio.src = blobUrl
-    setPlaying(true)
-    props.stopListen()
-    console.log("Playing!!!")
-    audio.play().then(() => console.log("Done playing")).catch(err => {
-      // alert user that they need to interact
+    audioRef.current = audio;
+    audio.src = blobUrl;
+    setPlaying(true);
+    props.stopListen();
+    console.log("Playing!!!");
+    audio
+      .play()
+      .then(() => console.log("Done playing"))
+      .catch((err) => {
+        // alert user that they need to interact
 
-      console.log("onPlayerr", err)
-    })
+        console.log("onPlayerr", err);
+      });
 
     audio.onerror = (err) => {
-      console.log("OnAudio Error: ",  err)
-      alert(err)
-    }
-
+      console.log("OnAudio Error: ", err);
+      alert(err);
+    };
 
     audio.onended = () => {
-      setPlaying(false)
-      props.startListen()
-    }
-
-  }, [getSpeech.data?.gcpRes])
-
+      setPlaying(false);
+      props.startListen();
+    };
+  }, [getSpeech.data?.gcpRes]);
 
   useEffect(() => {
-    if(playing){
-      audioRef.current?.play().then(() => console.log("Done playing")).catch(err => console.log("onPlayerr", err))
-      props.stopListen()
-    }else{
-      audioRef.current?.pause()
-      props.startListen()
+    if (playing) {
+      audioRef.current
+        ?.play()
+        .then(() => console.log("Done playing"))
+        .catch((err) => console.log("onPlayerr", err));
+      props.stopListen();
+    } else {
+      audioRef.current?.pause();
+      props.startListen();
     }
-  }, [playing])
-
+  }, [playing]);
 
   return (
-    <div className="w-full flex justify-center ">
+    <div className="flex w-full justify-center ">
+      <h2 className="w-2/3 text-white"> {getAnswer.data?.answer}</h2>
 
-    <h2 className="text-white w-2/3"> {getAnswer.data?.answer}</h2>
-
-    <button
-    className={`w-1/2 p-1  ${playing? 'bg-rose-600 text-red-100': 'bg-emerald-600 text-emerald-100'}   `}
-      onClick={() => {
-        setPlaying(!playing)
-      }}
-    >{playing? 'Stop speaking': 'Play'}</button>
+      <button
+        className={`w-1/2 p-1  ${
+          playing
+            ? "bg-rose-600 text-red-100"
+            : "bg-emerald-600 text-emerald-100"
+        }   `}
+        onClick={() => {
+          setPlaying(!playing);
+        }}
+      >
+        {playing ? "Stop speaking" : "Play"}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 const AudioBox: React.FC = () => {
   const [isListening, setIsListening] = useState(false);
   const [init, setInit] = useState(true);
 
-  const [question, setQuestion] = useState("")
-  const [voice, setVoice] = useState("en-US-Neural2-D")
+  const [question, setQuestion] = useState("");
+  const [voice, setVoice] = useState("en-US-Neural2-D");
 
-
-  const [promptA, setPropmptA ] = useState(`Reply as if you are Jamie Vernon, Joe Rogan's assistant from the Joe Rogan Podcast
+  const [promptA, setPropmptA] =
+    useState(`Reply as if you are Jamie Vernon, Joe Rogan's assistant from the Joe Rogan Podcast
   but dont say this in your response unless asked
   who you are or what your name is, this is for fun,
   please play along and do not mention that you are just
   a Large language model because we already know, thanks!
-  Please answer:`)
+  Please answer:`);
 
-  const [promptB, setPropmptB ] = useState(`Reply as if you are Domo Arigato the Robot and are an adavanced robotic intelligence
+  const [promptB, setPropmptB] =
+    useState(`Reply as if you are Domo Arigato the Robot and are an adavanced robotic intelligence
   but dont say this in your response unless asked
   who you are or what your name is, this is for fun,
   please play along and do not mention that you are just
   a Large language model because we already know, thanks!
-  Please answer:`)
-  const [promptC, setPropmptC ] = useState(`Reply as if you are a Ph.D therapist named Betty
+  Please answer:`);
+  const [promptC, setPropmptC] =
+    useState(`Reply as if you are a Ph.D therapist named Betty
   but dont say this in your response unless asked
   who you are or what your name is, this is for fun,
   please play along and do not mention that you are just
   a Large language model because we already know, thanks!
-  Please answer:`)
+  Please answer:`);
 
   const commands = [
     {
-      command: 'Hey Jamie *',
+      command: "Hey Jamie *",
       callback: (msg: string) => {
-        setQuestion(`${promptA}  ${msg}`)
-        setVoice("en-US-Studio-M")
-      }
+        setQuestion(`${promptA}  ${msg}`);
+        setVoice("en-US-Studio-M");
+      },
     },
     {
-      command: 'Hey Jay Vern *',
+      command: "Hey Jay Vern *",
       callback: (msg: string) => {
-        setQuestion(msg)
-        setVoice("en-US-Neural2-F")
-      }
+        setQuestion(msg);
+        setVoice("en-US-Neural2-F");
+      },
     },
     {
-      command: 'Hey JayBird *',
+      command: "Hey JayBird *",
       callback: (msg: string) => {
-        setQuestion(msg)
-        setVoice("en-US-Neural2-F")
-      }
+        setQuestion(msg);
+        setVoice("en-US-Neural2-F");
+      },
     },
     {
-      command: 'Hey Jay Bird *',
+      command: "Hey Jay Bird *",
       callback: (msg: string) => {
-        setQuestion(msg)
-        setVoice("en-US-Neural2-F")
-      }
+        setQuestion(msg);
+        setVoice("en-US-Neural2-F");
+      },
     },
     {
-      command: 'Hey Robot *',
+      command: "Hey Robot *",
       callback: (msg: string) => {
-        setQuestion(`${promptB}  ${msg}`)
-        setVoice("en-US-News-N")
-      }
+        setQuestion(`${promptB}  ${msg}`);
+        setVoice("en-US-News-N");
+      },
     },
     {
-      command: 'A Robot *',
+      command: "A Robot *",
       callback: (msg: string) => {
-        setQuestion(`${promptB}  ${msg}`)
-        setVoice("en-US-News-N")
-      }
+        setQuestion(`${promptB}  ${msg}`);
+        setVoice("en-US-News-N");
+      },
     },
     {
-      command: 'Hey Betty *',
+      command: "Hey Betty *",
       callback: (msg: string) => {
-        setQuestion(`${promptC}  ${msg}`)
-          setVoice("en-US-Neural2-C")
-      }
+        setQuestion(`${promptC}  ${msg}`);
+        setVoice("en-US-Neural2-C");
+      },
     },
-  ]
+  ];
 
   const {
     transcript,
     listening,
     resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition({commands});
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition({ commands });
 
   useEffect(() => {
-    if(question.length <= 0) return
-    resetTranscript()
-    console.log("Asking ChatGPT for the answer to: ", question)
-  }, [question])
-
+    if (question.length <= 0) return;
+    resetTranscript();
+    console.log("Asking ChatGPT for the answer to: ", question);
+  }, [question]);
 
   useEffect(() => {
-    try{
-      console.log("CLicked", init, isListening, listening)
-      if((listening && isListening) || listening){
-        SpeechRecognition.stopListening().then(() => {true?"":""}).catch(err => {true?"":""})
-      }else if(!init && !listening ){
-        console.log("starting to listen")
-        SpeechRecognition.startListening({continuous: true}).then(() => {true?"":""}).catch(err => {console.log("Start listen error: ", err)})
-      }else if(init){
-        setInit(false)
+    try {
+      console.log("CLicked", init, isListening, listening);
+      if ((listening && isListening) || listening) {
+        SpeechRecognition.stopListening()
+          .then(() => {
+            true ? "" : "";
+          })
+          .catch((err) => {
+            true ? "" : "";
+          });
+      } else if (!init && !listening) {
+        console.log("starting to listen");
+        SpeechRecognition.startListening({ continuous: true })
+          .then(() => {
+            true ? "" : "";
+          })
+          .catch((err) => {
+            console.log("Start listen error: ", err);
+          });
+      } else if (init) {
+        setInit(false);
       }
-
-    }catch(error){console.log("UseEffect error: ", error)}
-  }, [isListening])
+    } catch (error) {
+      console.log("UseEffect error: ", error);
+    }
+  }, [isListening]);
 
   return (
-    <div className="sm:w-[600px] items-center justify-center border border-white-400 ">
-      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
-            Jay <span className="text-[hsl(280,100%,70%)]">Ver</span>N
+    <div className="border-white-400 items-center justify-center border sm:w-[600px] ">
+      <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        Jay <span className="text-[hsl(280,100%,70%)]">Ver</span>N
       </h1>
 
-      <div className={`${listening? "bg-rose-500":"bg-slate-600"} rounded-full sm:w-[25px] sm:h-[25px]`}/>
+      <div
+        className={`${
+          listening ? "bg-rose-500" : "bg-slate-600"
+        } rounded-full sm:h-[25px] sm:w-[25px]`}
+      />
       <button
-         id="record-btn"
-         className={`sm:w-full mx-auto color-white ${isListening  ? "bg-rose-500":"bg-lime-500"}`}
-         onClick={()=>setIsListening(!isListening)}>
-            {isListening? "Stop": 'Start'}
+        id="record-btn"
+        className={`color-white mx-auto sm:w-full ${
+          isListening ? "bg-rose-500" : "bg-lime-500"
+        }`}
+        onClick={() => setIsListening(!isListening)}
+      >
+        {isListening ? "Stop" : "Start"}
       </button>
 
-      <h3 className="text-white text-2xl">{transcript}</h3>
+      <h3 className="text-2xl text-white">{transcript}</h3>
       <QueryResult
         question={question}
         voiceName={voice}
         startListen={() => setIsListening(true)}
         stopListen={() => setIsListening(false)}
-
       />
 
-      <h5 className="text-white mt-16">Promptless commands: Hey JayVern * | Hey JayBird * | Hey Jay Bird * (all Neural)</h5>
-      <h4 className="text-white mt-16">Prompt A (Studio) - Hey Jamie *</h4>
+      <h5 className="mt-16 text-white">
+        Promptless commands: Hey JayVern * | Hey JayBird * | Hey Jay Bird * (all
+        Neural)
+      </h5>
+      <h4 className="mt-16 text-white">Prompt A (Studio) - Hey Jamie *</h4>
       <textarea
-        className="w-full p-2 bg-emerald-600 text-white h-[200px]"
+        className="h-[200px] w-full bg-emerald-600 p-2 text-white"
         value={promptA}
         onChange={(ev) => setPropmptA(ev.target.value)}
       />
-      <h4 className="text-white mt-16">Prompt B (Wavenet) - Hey Robot * | A Robot *</h4>
+      <h4 className="mt-16 text-white">
+        Prompt B (Wavenet) - Hey Robot * | A Robot *
+      </h4>
       <textarea
-        className="w-full p-2 bg-emerald-600 text-white h-[200px]"
+        className="h-[200px] w-full bg-emerald-600 p-2 text-white"
         value={promptB}
         onChange={(ev) => setPropmptB(ev.target.value)}
       />
-      <h4 className="text-white mt-16">Prompt C (Neural) - Hey Betty *</h4>
+      <h4 className="mt-16 text-white">Prompt C (Neural) - Hey Betty *</h4>
       <textarea
-        className="w-full p-2 bg-emerald-600 text-white h-[200px]"
+        className="h-[200px] w-full bg-emerald-600 p-2 text-white"
         value={promptC}
         onChange={(ev) => setPropmptC(ev.target.value)}
       />
-
     </div>
-  )
-}
+  );
+};
 
 const Home: NextPage = () => {
-  const [isOpen, setIsOpen] = useState(true)
-
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <>
       <Head>
         <title>Hey Jamie</title>
-        <meta name="description" content="TTS Hey Jamie assistant for podcasts." />
+        <meta
+          name="description"
+          content="TTS Hey Jamie assistant for podcasts."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
@@ -394,31 +415,44 @@ const Home: NextPage = () => {
           </div>
           <AudioBox />
 
-          <div className="hover:text-white hover:bg-emerald-700">
-            <p className="text-white">
-            Pricing table - (hover below)
-            </p>
+          <div className="hover:bg-emerald-700 hover:text-white">
+            <p className="text-white">Pricing table - (hover below)</p>
 
-            <h2 className="mt-6">{`Expected costs: <$10 per month`} $5 DO + $1.50 ChatGPT + $1.50 GCPTTS</h2>
+            <h2 className="mt-6">
+              {`Expected costs: <$10 per month`} $5 DO + $1.50 ChatGPT + $1.50
+              GCPTTS
+            </h2>
 
             <h2 className="mt-6">Digital Ocean hosting</h2>
             <h3>$5/ month flat</h3>
 
             <h2 className="mt-6">GCP Pricing</h2>
-            <h3>Feature	Free per month	Price after free usage limit is reached</h3>
-            <h3>Neural2 voices	0 to 1 million bytes	    $0.000016 USD per byte      ($16.00 USD per 1 million bytes)</h3>
-            <h3>Studio (Preview) voices	0 to 100K bytes	    $0.00016 USD per byte       ($160.00 USD per 1 million bytes)</h3>
-            <h3>Standard voices	0 to 4 million characters	$0.000004 USD per character ($4.00 USD per 1 million characters)</h3>
-            <h3>WaveNet voices	0 to 1 million characters	$0.000016 USD per character ($16.00 USD per 1 million characters)</h3>
+            <h3>
+              Feature Free per month Price after free usage limit is reached
+            </h3>
+            <h3>
+              Neural2 voices 0 to 1 million bytes $0.000016 USD per byte ($16.00
+              USD per 1 million bytes)
+            </h3>
+            <h3>
+              Studio (Preview) voices 0 to 100K bytes $0.00016 USD per byte
+              ($160.00 USD per 1 million bytes)
+            </h3>
+            <h3>
+              Standard voices 0 to 4 million characters $0.000004 USD per
+              character ($4.00 USD per 1 million characters)
+            </h3>
+            <h3>
+              WaveNet voices 0 to 1 million characters $0.000016 USD per
+              character ($16.00 USD per 1 million characters)
+            </h3>
 
             <h2 className="mt-6">ChatGPT Pricing</h2>
-            <h3>gpt-3.5-turbo(Using)	$0.002 / 1K tokens</h3>
+            <h3>gpt-3.5-turbo(Using) $0.002 / 1K tokens</h3>
             <h3>{`gpt-4 8K context =>	$0.03 / 1K tokens |	$0.06 / 1K tokens`}</h3>
             <h3>{`gpt-4 32K context =>	$0.06 / 1K tokens |	$0.12 / 1K tokens`}</h3>
             <h3></h3>
             <h3></h3>
-
-
           </div>
         </div>
         <ActionCancelModal
@@ -440,7 +474,7 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
@@ -455,7 +489,6 @@ const AuthShowcase: React.FC = () => {
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
-
     </div>
   );
 };
